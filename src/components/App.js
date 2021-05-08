f6ff891a94db8c0aa4da5a01a6ab339957d72516
Lucas <lucas.wang.to@gmail.com>
2021-05-08 12:24:52 -0400

    Added initial Leaflet files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Map from './Map'
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Map />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Map = () => {
+    return <h1>Here lies the map!</h1>;
+}
+
+export default Map;

